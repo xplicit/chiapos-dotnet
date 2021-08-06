3bb9f2277df9b37dd207dea12b986214b7274eff
xplicit <svg@ngs.ru>
2021-08-07 03:36:22 +0700

    Make program arguments required

--- src/Chiapos.Dotnet/Program.cs
+++ src/Chiapos.Dotnet/Program.cs
@@ -12,37 +12,37 @@ namespace Chiapos.Dotnet
             var p = new OptionSet
             {
                 {
-                    "k|size", "Plot size", v => startupData.k = byte.Parse(v)
+                    "k|size=", "Plot size", v => startupData.k = byte.Parse(v)
                 },
                 {
-                    "r|threads", "Number of threads", v => startupData.num_threads = byte.Parse(v)
+                    "r|threads=", "Number of threads", v => startupData.num_threads = byte.Parse(v)
                 },
                 {
-                    "u|buckets", "Number of buckets", v => startupData.num_buckets = uint.Parse(v)
+                    "u|buckets=", "Number of buckets", v => startupData.num_buckets = uint.Parse(v)
                 },
                 {
-                    "s|stripes", "Size of stripes", v => startupData.num_stripes = ulong.Parse(v)
+                    "s|stripes=", "Size of stripes", v => startupData.num_stripes = ulong.Parse(v)
                 },
                 {
-                    "t|tempdir", "Temporary directory", v => startupData.tempdir = v
+                    "t|tempdir=", "Temporary directory", v => startupData.tempdir = v
                 },
                 {
-                    "2|tempdir2", "Second temporary directory", v => startupData.tempdir2 = v
+                    "2|tempdir2=", "Second temporary directory", v => startupData.tempdir2 = v
                 },
                 {
-                    "d|finaldir", "Final directory", v => startupData.finaldir = v
+                    "d|finaldir=", "Final directory", v => startupData.finaldir = v
                 },
                 {
-                    "f|file", "Filename", v => startupData.filename = v
+                    "f|file=", "Filename", v => startupData.filename = v
                 },
                 {
-                    "m|memo", "Memo to insert into the plot", v => startupData.memo = v
+                    "m|memo=", "Memo to insert into the plot", v => startupData.memo = v
                 },
                 {
-                    "i|id", "Unique 32-byte seed for the plot", v => startupData.id = v
+                    "i|id=", "Unique 32-byte seed for the plot", v => startupData.id = v
                 },
                 {
-                    "b|buffer", "Megabytes to be used as buffer for sorting and plotting", v => startupData.buffmegabytes = uint.Parse(v)
+                    "b|buffer=", "Megabytes to be used as buffer for sorting and plotting", v => startupData.buffmegabytes = uint.Parse(v)
                 },
                 {
                     "p|progress", "Display progress percentage during plotting", v => startupData.show_progress = v != null
